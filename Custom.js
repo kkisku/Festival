baa4677976201b42be5c2f351035c9c381f4c1cf
kkisku <kiran2kisku@gmail.com>
2018-08-19 18:27:58 +0530

    Add files via upload

---
+++ Custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#hid").fadeIn(3000);
+		$("#div1").hide();
+		$("#btn1").hide();
+		$("p").show();	
+		$("#hid").show();	
+		$("body").css("background-color","#FFE4E1");
+		$("img").show();
+		
+    });
+});
